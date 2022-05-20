0fd8e4b23a7c3bfa37bdbf169e557cb5e893999f
AKclown <13413629530@139.com>
2022-05-20 14:13:45 +0800

    jwt的封装

--- src/config/env/dev.js
+++ src/config/env/dev.js
@@ -19,5 +19,8 @@ module.exports = {
     redisConfig: {
         port: '6379',
         host: '127.0.0.1'
-    }
+    },
+
+    // jwt 过期时间
+    jwtExpiresIn: '1d', // 1. 字符串，如 '1h' '2d'； 2. 数字，单位是 s
 }
